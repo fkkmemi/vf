67333f700e86ce4bd8a57073e4f39fe30e883729
fkkmemi <fkkmemi@gmail.com>
2019-07-01 00:05:29 +0900

    10 vuetify로 네비게이션 드라워 만들기

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,41 +1,13 @@
 <template>
-  <v-list>
-    <v-list-tile v-for="item in items" :key="item.title">
-      <v-list-tile-avatar>
-        <v-icon>{{item.icon}}</v-icon>
-      </v-list-tile-avatar>
-      <v-list-tile-content>
-        <v-list-tile-title>{{item.title}}</v-list-tile-title>
-        <!-- <v-list-tile-sub-title>bbbbb</v-list-tile-sub-title> -->
-      </v-list-tile-content>
-      <!-- <v-list-tile-action>
-        <v-btn icon ripple>
-          <v-icon color="grey lighten-1">mdi-alert-box</v-icon>
-        </v-btn>
-      </v-list-tile-action> -->
-
-    </v-list-tile>
-
-  </v-list>
+  <div>
+    about
+  </div>
 </template>
 <script>
 export default {
   data () {
     return {
-      items: [
-        {
-          icon: 'mdi-alert',
-          title: 'home'
-        },
-        {
-          icon: 'mdi-alert-box',
-          title: 'About2'
-        },
-        {
-          icon: 'mdi-alert-circle',
-          title: 'About'
-        }
-      ]
+
     }
   }
 }
