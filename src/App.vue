00d7d34d1ad350925f0d3e7f25706130b9b13ef8
fkkmemi <fkkmemi@gmail.com>
2019-07-01 23:04:47 +0900

    11 vuetify로 하위 메뉴 구성하기

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,36 @@
       <v-toolbar flat color="transparent">
         <v-toolbar-title>Account</v-toolbar-title>
       </v-toolbar>
-      <v-divider></v-divider>
       <v-list>
-        <v-list-tile v-for="item in items" :key="item.title" :to="item.to">
-          <v-list-tile-avatar>
-            <v-icon>{{item.icon}}</v-icon>
-          </v-list-tile-avatar>
-          <v-list-tile-content>
-            <v-list-tile-title>{{item.title}}</v-list-tile-title>
-            <!-- <v-list-tile-sub-title>bbbbb</v-list-tile-sub-title> -->
-          </v-list-tile-content>
-          <!-- <v-list-tile-action>
-            <v-btn icon ripple>
-              <v-icon color="grey lighten-1">mdi-alert-box</v-icon>
-            </v-btn>
-          </v-list-tile-action> -->
+        <v-list-group
+          v-for="item in items"
+          :key="item.title"
+          v-model="item.active"
+          :prepend-icon="item.icon"
+          no-action
+        >
+          <template v-slot:activator>
+            <v-list-tile>
+              <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </template>
+          <v-list-tile
+            v-for="subItem in item.subItems"
+            :key="subItem.title"
+            :to="subItem.to"
+          >
+            <v-list-tile-content>
+              <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+            </v-list-tile-content>
 
-        </v-list-tile>
+            <v-list-tile-action>
+              <v-icon>{{ subItem.icon }}</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
 
+        </v-list-group>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar color="lime">
@@ -52,17 +64,32 @@ export default {
         {
           icon: 'mdi-alert',
           title: 'home',
-          to: '/'
+          active: true,
+          subItems: [
+            {
+              title: 'dashboard',
+              to: '/'
+            },
+            {
+              title: 'About2',
+              to: '/about2'
+            }
+          ]
         },
         {
           icon: 'mdi-alert-box',
-          title: 'About2',
-          to: '/about2'
-        },
-        {
-          icon: 'mdi-alert-circle',
-          title: 'About',
-          to: '/about'
+          title: 'Lectures',
+          active: false,
+          subItems: [
+            {
+              title: 'card',
+              to: '/lectures/card'
+            },
+            {
+              title: 'layout',
+              to: '/lectures/layout'
+            }
+          ]
         }
       ]
     }
