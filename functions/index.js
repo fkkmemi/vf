93c04fa45c2b5dca2e49b40f9b3b50a8a60b6ec4
fkkmemi <fkkmemi@gmail.com>
2019-07-09 22:29:57 +0900

    20 firebase functions RESTful api 만들기

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,5 @@ const functions = require('firebase-functions')
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!')
 })
+
+exports.test = functions.https.onRequest(require('./test'))
---
+++ functions/test/index.js
@@ -0,0 +1,23 @@
+const app = require('express')()
+
+app.post('/', (req, res) => {
+  res.send('post ok')
+})
+
+app.get('/', (req, res) => {
+  res.send('get ok')
+})
+
+app.get('/:id', (req, res) => {
+  res.send('get ok ' + req.params.id)
+})
+
+app.put('/:id', (req, res) => {
+  res.send('put ok ' + req.params.id)
+})
+
+app.delete('/:id', (req, res) => {
+  res.send('delete ok ' + req.params.id)
+})
+
+module.exports = app
