1c700dcd017469a7dc6212ca5c57f0f45da556e1
fkkmemi <fkkmemi@gmail.com>
2019-07-21 23:46:38 +0900

    31 로그인 상태 만들어보기

--- src/App.vue
+++ src/App.vue
@@ -37,13 +37,54 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar color="indigo" dark>
-      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>{{ $store.state.user ? $store.state.user.displayName : '아직 로그인 안함' }}</v-toolbar-title>
+      <v-toolbar-side-icon @click="drawer = !drawer" v-if="$store.state.user"></v-toolbar-side-icon>
+      <v-toolbar-title>미정 0.0.1</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn icon @click="signOut">
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
+      <v-toolbar-items v-if="$store.state.user">
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              icon
+              v-on="on"
+            >
+              <v-avatar
+                size="32"
+                color="grey lighten-4"
+              >
+                <img :src="$store.state.user.photoURL" alt="avatar">
+              </v-avatar>
+            </v-btn>
+          </template>
+          <v-card width="320">
+            <v-container grid-list-md>
+              <v-layout row wrap>
+                <v-flex xs4>
+                  <v-avatar
+                    size="96"
+                    color="grey lighten-4"
+                  >
+                    <img :src="$store.state.user.photoURL" alt="avatar">
+                  </v-avatar>
+                </v-flex>
+                <v-flex xs8>
+                  <v-card-text>
+                    <span class="font-weight-bold"> {{$store.state.user.displayName}}</span>
+                    <br>
+                    <span class="font-weight-thin">{{$store.state.user.email}}</span>
+                  </v-card-text>
+                </v-flex>
+              </v-layout>
+
+            </v-container>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" @click="$firebase.auth().signOut()">로그아웃</v-btn>
+
+            </v-card-actions>
+
+          </v-card>
+        </v-menu>
       </v-toolbar-items>
     </v-toolbar>
 
@@ -111,11 +152,6 @@ export default {
     }
   },
   methods: {
-    async signOut () {
-      // const r = await this.$firebase.auth().signOut()
-      // console.log(r)
-      this.$Progress.start()
-    }
   }
 }
 </script>
