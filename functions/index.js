9d71096896c928915cbe1cf2d282e84d35360b54
fkkmemi <fkkmemi@gmail.com>
2019-07-26 22:51:13 +0900

    36 firebase functions 환경변수 사용하기

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,12 @@
 const functions = require('firebase-functions')
+const admin = require('firebase-admin')
 
 // // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
+
+admin.initializeApp()
+
+console.log(functions.config().admin.email)
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!')
